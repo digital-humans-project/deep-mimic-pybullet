581fad61a53e8eed566cae4e10beb48a81157c16
Howyoung <howyoung3@gmail.com>
2023-05-28 14:31:22 +0200

    fix: arg env var

--- jobs/deep_mimic.sh
+++ jobs/deep_mimic.sh
@@ -4,11 +4,8 @@
 #SBATCH --time=24:00:00                    # hours:minutes:seconds
 #SBATCH --mem-per-cpu=2000
 #SBATCH --tmp=4000                        # per node!!
-#SBATCH --job-name=deep_mimic_$motion
-#SBATCH --output=./out/deep_mimic_$motion.out
-#SBATCH --error=./out/deep_mimic_$motion.err
 #SBATCH --gpus=1
 
 source scripts/setup.sh
 
-xvfb-run -a --server-args="-screen 0 480x480x24" python -m model.main -wb -vr -c conf/pybullet_env.json -a "run_humanoid3d_$motion_args.txt"
+xvfb-run -a --server-args="-screen 0 480x480x24" python -m model.main -wb -vr -c conf/pybullet_env.json -a "run_humanoid3d_${motion}_args.txt"
