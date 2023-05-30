4ed86ddf89c083a48327d4b3a8bb98a40269c0ce
Howyoung <howyoung3@gmail.com>
2023-05-30 16:29:30 +0200

    feat: save video by length

--- model/envs/video_recoder.py
+++ model/envs/video_recoder.py
@@ -1,12 +1,16 @@
-import logging
 import heapq
+import logging
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 import numpy as np
-
-from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvObs, VecEnvStepReturn, VecEnvWrapper
 from gym.wrappers.monitoring.video_recorder import ImageEncoder
+from stable_baselines3.common.vec_env.base_vec_env import (
+    VecEnv,
+    VecEnvObs,
+    VecEnvStepReturn,
+    VecEnvWrapper,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -33,11 +37,15 @@ class VecVideoRecorder(VecEnvWrapper):
             buffer = self.frame_buffers[i]
             buffer.append(frame)
             if done:
+                total_rew = np.sum(rew)
+                length = len(buffer)
                 if len(self.best_videos) == self.topk:
-                    rew, _ = heapq.heappushpop(self.best_videos, (np.sum(rew), buffer))
-                    logger.info(f"Dropped video with reward {rew}")
+                    if length > self.best_videos[0][0]:
+                        l, r, _ = heapq.heappushpop(self.best_videos, (length, total_rew, buffer))
+                        logger.info(f"Dropped video with reward = {r}, length = {l}")
                 else:
-                    heapq.heappush(self.best_videos, (np.sum(rew), buffer))
+                    heapq.heappush(self.best_videos, (length, total_rew, buffer))
+                logger.info(f"New video with reward {total_rew}, length = {length}")
                 self.frame_buffers[i] = []
                 new_obs = env.reset()
                 obs[i] = new_obs
@@ -46,9 +54,9 @@ class VecVideoRecorder(VecEnvWrapper):
     def save_videos(self, save_path: Union[str, Path], fps: int, output_fps: int) -> None:
         save_path = Path(save_path)
         save_path.mkdir(exist_ok=True, parents=True)
-        for i, (_, buffer) in enumerate(self.best_videos):
+        for i, (l, rew, buffer) in enumerate(self.best_videos):
             enc = ImageEncoder(
-                str(save_path / f"video_{i}.mp4"),
+                str(save_path / f"video_{i}_rew_{rew:02f}_{l/fps:.01f}s.mp4"),
                 frame_shape=buffer[0].shape,
                 frames_per_sec=fps,
                 output_frames_per_sec=output_fps,
@@ -56,3 +64,6 @@ class VecVideoRecorder(VecEnvWrapper):
             for frame in buffer:
                 enc.capture_frame(frame)
             enc.close()
+
+    def render(self, mode: str = "human"):
+        VecEnv.render(self, mode=mode)
